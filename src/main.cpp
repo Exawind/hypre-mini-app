8f99d5891765b9dea50ec0b9960d70e8d4ffa68b
PaulMullowney <pmullown@nrel.gov>
2021-04-15 14:28:35 -0600

    An attempt to assign devices properly.

--- src/main.cpp
+++ src/main.cpp
@@ -7,24 +7,54 @@
 #include <iostream>
 #include <chrono>
 
+int getNodeCount(int nproc, int device_count)
+{
+  int rank, is_rank0, nodes;
+  MPI_Comm shmcomm;
+
+  MPI_Comm_split_type(MPI_COMM_WORLD, MPI_COMM_TYPE_SHARED, 0,
+		      MPI_INFO_NULL, &shmcomm);
+  MPI_Comm_rank(shmcomm, &rank);
+  is_rank0 = (rank == 0) ? 1 : 0;
+  MPI_Allreduce(&is_rank0, &nodes, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+  MPI_Comm_free(&shmcomm);
+
+  // Here we make sure that we don't try to mod with more than the actual number of devices.
+  int ngpus_to_use = nproc/nodes;
+  ngpus_to_use = ngpus_to_use < device_count ? ngpus_to_use : device_count;
+  
+  // Here we assign device based on the rank in the shmcomm, not the global.
+  int device = rank%ngpus_to_use;
+
+  return device;
+}
+
 int main(int argc, char* argv[])
 {
-    int iproc;
+  int iproc, nproc;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &iproc);
-
-    HYPRE_Int ret = HYPRE_Init();
+    MPI_Comm_size(MPI_COMM_WORLD, &nproc);
 
 #ifdef HAVE_CUDA
-    int count, device;
+    int count;
     cudaGetDeviceCount(&count);
-    //cudaSetDevice(iproc_ % count);
+
+    // get the device from
+    int device = getNodeCount(nproc, count);
+
+    // set the device before calling HypreInit. 
+    cudaSetDevice(device);
     cudaGetDevice(&device);
     size_t free, total;
     cudaMemGetInfo(&free, &total);
     printf("\trank=%d : %s %s %d : device=%d of %d : free memory=%1.8g GB, total memory=%1.8g GB\n",
 	   iproc,__FUNCTION__,__FILE__,__LINE__,device,count,free/1.e9,total/1.e9);
+#endif
 
+    HYPRE_Int ret = HYPRE_Init();
+
+#ifdef HAVE_CUDA
     hypre_HandleDefaultExecPolicy(hypre_handle()) = HYPRE_EXEC_DEVICE;
     hypre_HandleSpgemmUseCusparse(hypre_handle()) = 0;
 #endif
