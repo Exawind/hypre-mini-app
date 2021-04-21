e011b20020a1adebef2fa0a095853826a2eff744
PaulMullowney <pmullown@nrel.gov>
2021-04-21 16:03:08 -0600

    removing these prints

--- cmake/FindUmpire.cmake
+++ cmake/FindUmpire.cmake
@@ -7,14 +7,10 @@
 #   - UMPIRE_LIBRARIES
 #
 
-message(${UMPIRE_DIR})
-
 find_path(UMPIRE_INCLUDE_DIRS
   umpire.h
   HINTS ${UMPIRE_DIR} ${CMAKE_INSTALL_PREFIX}
   PATH_SUFFIXES include/umpire/interface)
-
-message(${UMPIRE_INCLUDE_DIRS})
 
 find_library(UMPIRE_LIBRARIES
   NAMES umpire umpire
@@ -27,5 +23,3 @@ find_package_handle_standard_args(
 mark_as_advanced(UMPIRE_INCLUDE_DIRS UMPIRE_LIBRARIES)
 
 set(UMPIRE_INCLUDE_DIRS  "${UMPIRE_INCLUDE_DIRS}/../../")
-
-message(${UMPIRE_INCLUDE_DIRS})
